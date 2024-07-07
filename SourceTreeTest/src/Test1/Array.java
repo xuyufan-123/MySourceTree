5c4dd3e6065c7545e285f9ff10d9f89d6842e9d8
Edgardontknowhow <83048706+xuyufan-123@users.noreply.github.com>
2024-07-07 11:39:20 +0800

    Add files via upload

---
+++ SourceTreeTest/src/Test1/Array.java
@@ -0,0 +1,19 @@
+package Test1;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+        //int[] numbers = new int[];
+        //numbers[1]=0;
+
+        int[] numbers = {1,2,6,4,5};
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println(numbers.length);
+        int x= (int)2.0;
+    }
+}
